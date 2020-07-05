7e4e51aa3a0af2a48f150204d2991a5e0e6421a7
ghodouss <ghodouss@usc.edu>
2020-07-05 17:00:16 -0500

    updated ssl script

--- dev/ssl_cert.sh
+++ dev/ssl_cert.sh
@@ -2,5 +2,5 @@ sudo add-apt-repository ppa:certbot/certbot
 sudo apt update
 sudo apt install python-certbot-nginx
 sudo systemctl reload nginx
-sudo certbot --nginx -d fleek-fashion-engine.shop 
+sudo certbot --nginx -d fleek-fashion-engine.shop -d www.fleek-fashion-engine.shop
 
