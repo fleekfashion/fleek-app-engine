246aa3dbed4995d4ed85c9c1d15ecedd2ba4223b
ghodouss <ghodouss@usc.edu>
2021-02-25 16:55:35 -0600

    migrate push to sqlalchemy and support json dumps

--- src/utils/hashers.py
+++ src/utils/hashers.py
@@ -2,4 +2,4 @@ import pyhash
 
 def apple_id_to_user_id_hash(apple_id):
     hasher = pyhash.farm_fingerprint_64()
-    return hasher(apple_id) // 10
+    return hasher(apple_id) // 10 if apple_id is not None else None
