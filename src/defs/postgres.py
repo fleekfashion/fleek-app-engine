8d60caa90bafad894257ef64e9408ac817769a28
Kian Ghodoussi <ghodouss@usc.edu>
2021-04-30 17:11:06 -0700

    Revert "Feature: Boards API"

--- src/defs/postgres.py
+++ src/defs/postgres.py
@@ -3,8 +3,6 @@ from sqlalchemy.engine import Engine
 from functional import seq
 
 from src.defs.utils import PostgreTable
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.automap import automap_base, name_for_collection_relationship
 
 DATABASE_USER = "postgres"
 PASSWORD = "fleek-app-prod1"
@@ -14,7 +12,6 @@ PROJECT = 'staging'
 conn_str = f"postgres://{DATABASE_USER}:{PASSWORD}@localhost:5431/{DBNAME}"
 engine: Engine = create_engine(conn_str)
 metadata = MetaData(engine, schema=PROJECT)
-board_metadata = MetaData(engine, schema=PROJECT)
 
 ADVERTISER_PRODUCT_COUNT_TABLE = PostgreTable("advertiser_product_count", metadata, autoload=True)
 PRODUCT_INFO_TABLE = PostgreTable("product_info", metadata, autoload=True)
@@ -27,20 +24,3 @@ TOP_PRODUCTS_TABLE = PostgreTable("top_products", metadata, autoload=True)
 USER_EVENTS_TABLE = PostgreTable("user_events", metadata, autoload=True)
 USER_FAVED_BRANDS_TABLE = PostgreTable("user_faved_brands", metadata, autoload=True)
 USER_MUTED_BRANDS_TABLE = PostgreTable("user_muted_brands", metadata, autoload=True)
-BOARD_TABLE = PostgreTable("board", metadata, autoload=True)
-BOARD_TYPE_TABLE = PostgreTable("board_type", metadata, autoload=True)
-BOARD_PRODUCT_TABLE = PostgreTable("board_product", metadata, autoload=True)
-USER_BOARD_TABLE = PostgreTable("user_board", metadata, autoload=True)
-REJECTED_BOARD_TABLE = PostgreTable("rejected_board", metadata, autoload=True)
-
-def _name_for_collection_relationship(base, local_cls, referred_cls, constraint):
-    if constraint.name:
-        return constraint.name.lower()
-    # if this didn't work, revert to the default behavior
-    return name_for_collection_relationship(base, local_cls, referred_cls, constraint)
-
-## Map tables to objects
-Base = automap_base(metadata=metadata)
-Base.prepare(name_for_collection_relationship=_name_for_collection_relationship)
-ProductInfo, ProductSizeInfo = Base.classes.product_info, Base.classes.product_size_info
-Board, UserBoard, BoardType, BoardProduct = Base.classes.board, Base.classes.user_board, Base.classes.board_type, Base.classes.board_product
