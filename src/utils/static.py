ffc097fe943d767fca812b1819bec6c1701d4f36
Kian <ghodouss@usc.edu>
2021-05-25 16:14:02 -0700

    migrate static endpoints to session api

--- src/utils/static.py
+++ src/utils/static.py
@@ -1,26 +1,32 @@
+import typing as t
 import cachetools.func
 from functional import seq
+from src.utils.sqlalchemy_utils import session_scope
 from sqlalchemy.sql import select, func, asc
 
 from src.defs import postgres as p
 
 @cachetools.func.ttl_cache(ttl=60*60)
-def get_advertiser_counts():
-    query = p.ADVERTISER_PRODUCT_COUNT_TABLE \
-                .select()
-    res = p.engine.connect().execute(query)
+def get_advertiser_counts() -> t.Dict[str, int]:
+    with session_scope() as session:
+        res = session.query(
+            p.AdvertiserProductCount.advertiser_name,
+            p.AdvertiserProductCount.n_products,
+        ).all()
+
     advertiser_counts = seq(res) \
         .map(lambda x: (x['advertiser_name'], x['n_products'])) \
         .to_dict()
     return advertiser_counts
 
 @cachetools.func.ttl_cache(ttl=60*60)
-def get_advertiser_names():
-    query = p.ADVERTISER_PRODUCT_COUNT_TABLE \
-                .select() \
-                .with_only_columns([p.ADVERTISER_PRODUCT_COUNT_TABLE.c.advertiser_name]) \
-                .order_by(func.lower("advertiser_name"))
+def get_advertiser_names() -> t.List[str]:
+    with session_scope() as session:
+        res = session.query(
+            p.AdvertiserProductCount.advertiser_name,
+        ).all()
 
-    res = p.engine.connect().execute(query)
-    parsed_res = [ row["advertiser_name"] for row in res ]
-    return parsed_res
+    advertiser_counts = seq(res) \
+        .map(lambda x: (x['advertiser_name'])) \
+        .to_list()
+    return advertiser_counts
