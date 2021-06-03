2286b4a2ea65c8ad7630c7905962e05660f2b77d
Kian Ghodoussi <ghodouss@usc.edu>
2021-06-02 20:52:04 -0700

    Sqlalchemy 2 0 migration (#110)
    * complete refactor to 2.0 api
    
    * build run_query heper function
    
    * migrate to run_query module
    
    * rm guppy req
    
    * fix minor issue: cant run parallelizaiton with gunicorn
    
    * fix gunicorn bug

--- src/utils/query.py
+++ src/utils/query.py
@@ -2,7 +2,6 @@ import typing as t
 import random
 
 import sqlalchemy as s
-from sqlalchemy.orm.query import Query
 from sqlalchemy.sql.selectable import Alias, CTE, Select
 from sqlalchemy import subquery 
 from sqlalchemy import Column
@@ -21,24 +20,22 @@ def gen_rand() -> str:
     return "_" + str(random.randint(1, 10**8))
 
 def sort_columns(
-        session: Session,
         q: t.Union[Alias, CTE]
-    ) -> Query:
-    ordered_q = session.query(*[
+    ) -> Select:
+    ordered_q = s.select(*[
         c.label(c.name) for c in sorted(q.c, key=lambda x: x.name)
     ])
     return ordered_q
 
     
 def union_by_names(
-        session: Session,
         q1: t.Union[Alias, CTE], 
         q2: t.Union[Alias, CTE],
         union_all = False
-    ) -> Query:
+    ) -> Select:
 
-    ordered_q1 = sort_columns(session, q1)
-    ordered_q2 = sort_columns(session, q2)
+    ordered_q1 = sort_columns(q1)
+    ordered_q2 = sort_columns(q2)
 
     res = ordered_q1.union_all(ordered_q2) if union_all \
         else ordered_q1.union(ordered_q2)
@@ -46,11 +43,10 @@ def union_by_names(
 
         
 def apply_ranking(
-        session: Session, 
         products_subquery: t.Union[Alias, CTE], 
         user_id: int, 
         pct: float,
-    ) -> Query:
+    ) -> Select:
     def _get_scaling_factor(user_id: int, pct: float):
         n_advertisers = len(static.get_advertiser_names())
         n_fave_brands = len(user_info.get_user_fave_brands(user_id))
@@ -61,13 +57,13 @@ def apply_ranking(
     AC = p.AdvertiserProductCount
     scaling_factor = _get_scaling_factor(user_id, pct)
     
-    faved_brands = session.query(
+    faved_brands = s.select(
         p.UserFavedBrands.advertiser_name,
         literal(scaling_factor).label('scaling_factor')
     ).filter(p.UserFavedBrands.user_id == literal(user_id)) \
     .cte('fave_brands' + gen_rand())
 
-    ranked_products = session.query(
+    ranked_products = s.select(
         products_subquery,
         (F.random()*F.sqrt(AC.n_products) \
             *F.cbrt(AC.n_products) \
@@ -81,18 +77,17 @@ def apply_ranking(
     ).filter(products_subquery.c.advertiser_name == AC.advertiser_name) \
     .cte('ranked_products' + gen_rand())
 
-    final_q = session.query(ranked_products) \
+    final_q = s.select(ranked_products) \
                   .order_by(ranked_products.c.normalized_rank.asc())
     return final_q
 
 def _apply_filter(
-        session: Session,
         products_subquery: t.Union[Alias, CTE], 
         key: str,
         value: t.Any,
-    ) -> Query:
+    ) -> Select:
 
-    q = session.query(products_subquery)
+    q = s.select(products_subquery)
     subq = products_subquery
 
     if key == "min_price":
@@ -109,34 +104,33 @@ def _apply_filter(
     return q.cte(f"{key}_filter_applied" + gen_rand())
 
 def apply_filters(
-        session: Session,
         products_subquery: t.Union[Alias, CTE],
         args: dict,
         active_only: bool
-    ) -> Query:
+    ) -> Select:
 
     subq = products_subquery
     for key, value in args.items():
-        subq = _apply_filter(session, subq, key, value)
+        subq = _apply_filter(subq, key, value)
     
-    final_q = session.query(subq)
+    final_q = s.select(subq)
     if active_only:
         final_q = final_q.filter(subq.c.is_active == True)
     return final_q
 
-def join_product_color_info(session: Session, products_subquery: t.Union[Alias, CTE], product_id_field: str = 'product_id') -> Query:
+def join_product_color_info(products_subquery: t.Union[Alias, CTE], product_id_field: str = 'product_id') -> Select:
     join_field = products_subquery.c[product_id_field]
 
     ## Get the ids
-    alt_color_ids = session.query(
+    alt_color_ids = s.select(
         p.ProductColorOptions.product_id,
         p.ProductColorOptions.alternate_color_product_id,
     ).filter(
-        p.ProductColorOptions.product_id.in_(session.query(join_field))
+        p.ProductColorOptions.product_id.in_(s.select(join_field))
     ).cte('alt_color_ids_cte' + gen_rand())
 
     ## Get basic alt color info
-    alt_color_info = session.query(
+    alt_color_info = s.select(
         alt_color_ids.c.product_id,
         postgresql.array_agg(
             F.json_build_object(
@@ -151,7 +145,7 @@ def join_product_color_info(session: Session, products_subquery: t.Union[Alias, 
     .cte('alt_color_info_cte' + gen_rand())
     
     ## Join with original query
-    final_query = session.query(
+    final_query = s.select(
         products_subquery, 
         alt_color_info.c.product_color_options
     ).join(
@@ -161,10 +155,10 @@ def join_product_color_info(session: Session, products_subquery: t.Union[Alias, 
     )
     return final_query
 
-def join_product_sizes(session: Session, products_subquery: t.Union[Alias, CTE], product_id_field: str = 'product_id') -> Query:
+def join_product_sizes(products_subquery: t.Union[Alias, CTE], product_id_field: str = 'product_id') -> Select:
     join_field = products_subquery.c[product_id_field]
 
-    sizes_subquery = session.query(
+    sizes_subquery = s.select(
         p.ProductSizeInfo.product_id,
         postgresql.array_agg(
             F.json_build_object(
@@ -174,42 +168,38 @@ def join_product_sizes(session: Session, products_subquery: t.Union[Alias, CTE],
             )
         ).label('sizes')
     ).filter(
-        p.ProductSizeInfo.product_id.in_(session.query(join_field))
+        p.ProductSizeInfo.product_id.in_(s.select(join_field))
     ) \
      .group_by(p.ProductSizeInfo.product_id) \
      .cte('add_size_cte' + gen_rand())
 
-    return session.query(products_subquery, sizes_subquery.c.sizes) \
+    return s.select(products_subquery, sizes_subquery.c.sizes) \
                   .join(sizes_subquery, sizes_subquery.c.product_id == join_field, isouter=True)
 
 def join_external_product_info(
-        session: Session, 
         products_subquery: t.Union[Alias, CTE], 
         product_id_field: str = 'product_id'
-    ) -> Query:
+    ) -> Select:
 
     color_info_products_subquery = join_product_color_info(
-        session,
         products_subquery,
         product_id_field=product_id_field
     ).cte('products_add_color_info_cte' + gen_rand())
 
     parsed_products_query = join_product_sizes(
-        session, 
         color_info_products_subquery,
         product_id_field=product_id_field
     )
     return parsed_products_query
 
 def join_base_product_info(
-        session: Session, 
-        subquery: t.Union[Alias, CTE], 
+        subquery: t.Union[Alias, CTE],
         product_id_field: str = 'product_id'
-    ) -> Query:
+    ) -> Select:
     join_field = subquery.c[product_id_field]
 
     ## Base Product Info
-    products_query = session.query(
+    products_query = s.select(
             *[c for c in p.ProductInfo.__table__.c 
                 if c.name not in subquery.c ],
             subquery
@@ -217,21 +207,18 @@ def join_base_product_info(
     return products_query
 
 def join_product_info(
-        session: Session, 
         subquery: t.Union[Alias, CTE], 
         product_id_field: str = 'product_id'
-    ) -> Query:
+    ) -> Select:
 
     ## Base product info
     products_query = join_base_product_info(
-        session,
         subquery,
         product_id_field=product_id_field
     ).cte('base_product_info' + gen_rand())
 
     ## All external Product Info
     parsed_products_query = join_external_product_info(
-        session,
         products_query,
         product_id_field=product_id_field
     )
