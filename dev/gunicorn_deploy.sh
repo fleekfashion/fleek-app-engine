0ceaf60d3d3ee74a469f23f43315be478acc7a1b
Kian <ghodouss@usc.edu>
2021-06-02 20:39:53 -0700

    fix gunicorn bug

--- dev/gunicorn_deploy.sh
+++ dev/gunicorn_deploy.sh
@@ -1,1 +1,1 @@
-gunicorn main:app -b 0.0.0.0:5000 --log-level debug --max-requests 500 gunicorn main:app -b 0.0.0.0:5000 --log-level debug
+gunicorn main:app -b 0.0.0.0:5000 --log-level debug --max-requests 500
