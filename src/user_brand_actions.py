dfd9932fef659678f6a0277c270dcad79bafe4bd
Kian Ghodoussi <ghodouss@usc.edu>
2021-06-07 12:15:25 -0700

    Sql 2 0 migration (#117)
    * migrate boards api to 2.0
    
    * migrate user faved brands
    
    * finish sqlalchemy 20 migration
    
    * fix small bug
    
    * minor syntax fixes
    
    * manually commit transaction
    
    * small naming bugfix

--- src/user_brand_actions.py
+++ src/user_brand_actions.py
@@ -1,8 +1,12 @@
 from src.utils import hashers
 from src.defs import postgres as p
 from src.utils.user_info import pop_user_fave_brands 
+from src.utils.sqlalchemy_utils import session_scope
 
-def _add_brand_helper(table: p.PostgreTable, args: dict) -> bool:
+import sqlalchemy as s
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+
+def _add_brand_helper(table: DeclarativeMeta, args: dict) -> bool:
     new_args = {}
     user_id = hashers.apple_id_to_user_id_hash(args['user_id'])
 
@@ -11,45 +15,37 @@ def _add_brand_helper(table: p.PostgreTable, args: dict) -> bool:
     new_args['advertiser_name'] = args['advertiser_name']
     new_args['event_timestamp'] = args['event_timestamp']
 
-    query = table.insert(None).values(**new_args)
-    conn = p.engine.connect()
-    try:
-        conn.execute(query)
-    except Exception as e:
-        print(e)
-
+    with session_scope() as session:
+        session.add(table(**new_args))
+        session.commit()
     pop_user_fave_brands(user_id)
     return True
 
-def _remove_user_brand_helper(table: p.PostgreTable, args: dict) -> bool:
+def _remove_user_brand_helper(table: DeclarativeMeta, args: dict) -> bool:
     ## Required
     user_id = hashers.apple_id_to_user_id_hash(args['user_id'])
     advertiser_name = args['advertiser_name']
 
-    query = table.delete(None) \
-            .where(table.c.user_id == user_id) \
-            .where(table.c.advertiser_name == advertiser_name)
+    query = s.delete(table) \
+        .where(table.user_id == user_id) \
+        .where(table.advertiser_name == advertiser_name)
 
-    print(query)
-    conn = p.engine.connect()
-    try:
-        conn.execute(query)
-    except Exception as e:
-        print(e)
-
+    with session_scope() as session:
+        session.execute(query)
+        session.commit()
     pop_user_fave_brands(user_id)
     return True
 
 def write_user_faved_brand(args: dict) -> bool:
-    _remove_user_brand_helper(p.USER_MUTED_BRANDS_TABLE, args) ## TODO Hack convert this to 1 call
-    return _add_brand_helper(p.USER_FAVED_BRANDS_TABLE, args)
+    _remove_user_brand_helper(p.UserMutedBrands, args) ## TODO Hack convert this to 1 call
+    return _add_brand_helper(p.UserFavedBrands, args)
 
 def write_user_muted_brand(args: dict) -> bool:
-    _remove_user_brand_helper(p.USER_FAVED_BRANDS_TABLE, args)
-    return _add_brand_helper(p.USER_MUTED_BRANDS_TABLE, args)
+    _remove_user_brand_helper(p.UserFavedBrands, args)
+    return _add_brand_helper(p.UserMutedBrands, args)
 
 def rm_user_faved_brand(args: dict) -> bool:
-    return _remove_user_brand_helper(p.USER_FAVED_BRANDS_TABLE, args)
+    return _remove_user_brand_helper(p.UserFavedBrands, args)
 
 def rm_user_muted_brand(args: dict) -> bool:
-    return _remove_user_brand_helper(p.USER_MUTED_BRANDS_TABLE, args)
+    return _remove_user_brand_helper(p.UserMutedBrands, args)
