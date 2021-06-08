2beae223af056dfa92bb3eefff423799d2f612f1
Kian Ghodoussi <ghodouss@usc.edu>
2021-06-07 17:18:06 -0700

    read board bug fixes (#119)
    * read board bug fixes
    
    * filter out board id column from json
    
    * Fix sqlalchemy 2.0 migration for board endpoints
    
    Co-authored-by: Rami <rhammoud@usc.edu>

--- src/read_user_boards.py
+++ src/read_user_boards.py
@@ -7,7 +7,6 @@ from src.utils import hashers
 from src.defs import postgres as p
 from sqlalchemy.dialects import postgresql
 from sqlalchemy import func 
-import sqlalchemy as sqa
 import itertools
 
 def getBoardInfo(args: dict) -> dict:
@@ -25,7 +24,7 @@ def getBoardProductsBatch(args: dict) -> dict:
     board_pids_query = s.select(p.BoardProduct.product_id) \
                     .filter(p.BoardProduct.board_id == board_id) \
                     .order_by(p.BoardProduct.last_modified_timestamp.desc()) \
-                    .subquery(reduce_columns=True)
+                    .cte()
     products_batch = join_product_info(board_pids_query) \
         .limit(limit) \
         .offset(offset) 
@@ -39,49 +38,45 @@ def getUserBoardsBatch(args: dict) -> dict:
     offset = args['offset']
     limit = args['limit']
 
-    user_board_ids_subq = s.select(p.UserBoard) \
-        .with_entities(p.UserBoard.board_id) \
+    user_board_ids_subq = s.select(p.UserBoard.board_id, p.UserBoard.user_id) \
         .filter(p.UserBoard.user_id == user_id) \
         .order_by(p.UserBoard.last_modified_timestamp.desc()) \
         .offset(offset) \
         .limit(limit) \
-        .subquery()
+        .cte()
 
-
-    board_product_lateral_subq = s.select(p.BoardProduct) \
-        .with_entities(p.BoardProduct.board_id, p.BoardProduct.product_id) \
+    board_product_lateral_subq = s.select(p.BoardProduct.board_id, p.BoardProduct.product_id) \
         .filter(p.BoardProduct.board_id == user_board_ids_subq.c.board_id) \
         .order_by(p.BoardProduct.last_modified_timestamp.desc()) \
         .limit(6) \
         .subquery() \
         .lateral()
 
-    join_board_product_subq = s.select(user_board_ids_subq, board_product_lateral_subq) \
-        .join(board_product_lateral_subq, sqa.true()) \
-        .subquery(reduce_columns=True)
+    join_board_product_subq = s.select(board_product_lateral_subq, user_board_ids_subq.c.user_id) \
+        .join(board_product_lateral_subq, s.true()) \
+        .cte()
+    join_product_info_query = join_product_info(join_board_product_subq).cte()
 
-    join_product_info_query = join_product_info(s, join_board_product_subq).subquery()
-
-    product_cols = [(c.name, c) for c in join_product_info_query.c]
+    product_cols = [(c.name, c) for c in join_product_info_query.c if 'board_id' not in c.name ]
     product_cols_json_agg = list(itertools.chain(*product_cols))
     group_by_board_subq = s.select(
             join_product_info_query.c.board_id,
             postgresql.array_agg(
-                func.json_build_object(product_cols_json_agg)
+                func.json_build_object(*product_cols_json_agg)
             ).label('products')
         ) \
         .group_by(join_product_info_query.c.board_id) \
-        .subquery()
+        .cte()
 
     join_board_subq = s.select(p.Board.board_id, p.Board.name, p.Board.creation_date, p.Board.description, p.Board.artwork_url) \
         .join(user_board_ids_subq, user_board_ids_subq.c.board_id == p.Board.board_id) \
-        .subquery()
-
+        .cte()
     join_board_type_subq = s.select(join_board_subq, p.BoardType) \
         .join(join_board_subq, join_board_subq.c.board_id == p.BoardType.board_id) \
-        .subquery(reduce_columns=True)
+        .cte()
 
-    join_board_info_and_products = s.select(join_board_type_subq, group_by_board_subq) \
+    join_board_type_subq_cols = [c for c in join_board_type_subq.c if 'board_id' not in c.name ]
+    join_board_info_and_products = s.select(*join_board_type_subq_cols, group_by_board_subq) \
         .join(join_board_type_subq, group_by_board_subq.c.board_id == join_board_type_subq.c.board_id)
 
     result = run_query(join_board_info_and_products)
