c5cb274711b9373a4ebd3e46e2a3078474503a38
Kian <ghodouss@usc.edu>
2021-06-14 08:58:05 -0700

    increase num workers gunicorn

--- dev/gunicorn_deploy.sh
+++ dev/gunicorn_deploy.sh
@@ -1,1 +1,4 @@
-gunicorn main:app -b 0.0.0.0:5000 --log-level debug
+gunicorn main:app -b 0.0.0.0:5000 \
+  --log-level debug \
+  --timeout 45 \
+  --workers $(( 2*$(grep -c ^processor /proc/cpuinfo)+1 ))
