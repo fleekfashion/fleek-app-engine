65b04516c8df0995f2066a87306836fb881d29c7
Rami <rhammoud@usc.edu>
2021-07-16 18:14:08 -0700

    Price drop board endpoint to create price drop board for a specific user

--- main.py
+++ main.py
@@ -214,6 +214,12 @@ def createNewBoard():
     res = wub.create_new_board(data)
     return jsonify(res)
 
+@app.route('/createPriceDropBoard', methods=['POST'])
+def createPriceDropBoard():
+    data = request.get_json(force=True)
+    res = wub.create_price_drop_board(data)
+    return jsonify(res)
+
 @app.route('/updateBoardName', methods=['POST'])
 def updateBoardName():
     data = request.get_json(force=True)
--- src/write_user_boards.py
+++ src/write_user_boards.py
@@ -3,9 +3,11 @@ from src.utils import hashers
 from src.defs import postgres as p
 import uuid
 from datetime import datetime as dt
-from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.dialects.postgresql import insert, UUID
 import sqlalchemy as s
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.sql import Values
+from sqlalchemy.sql.expression import cast
 
 
 def create_new_board(args: dict) -> dict:
@@ -59,6 +61,93 @@ def create_new_board(args: dict) -> dict:
         return {"success": False}
     
     return {"success": True, "board_id": board_id}
+
+def create_price_drop_board(args: dict) -> dict:
+    board_id = uuid.uuid4().hex
+    user_id = hashers.apple_id_to_user_id_hash(args['user_id'])
+    last_modified_timestamp = int(dt.now().timestamp())
+    creation_date = dt.now().strftime('%Y-%m-%d')
+    board_name = args['board_name']
+
+    ## Required fields
+    board_args = {
+        'board_id':board_id,
+        'creation_date': creation_date,
+        'name': board_name,
+        'description': args.get('description', None),
+        'last_modified_timestamp': last_modified_timestamp,
+        'artwork_url': args.get('artwork_url', None),
+        'board_type': 'price_drop',
+    }
+
+    user_board_args = {
+        'board_id': board_id,
+        'user_id': user_id,
+        'last_modified_timestamp': last_modified_timestamp,
+        'is_owner': True,
+        'is_collaborator': False,
+        'is_following': False,
+        'is_suggested': False,
+    }
+    
+    get_price_drop_boards_for_user_id_cte = s.select(p.Board.board_type) \
+        .join(p.UserBoard, p.UserBoard.board_id == p.Board.board_id) \
+        .where(p.UserBoard.user_id == user_id) \
+        .cte()
+    
+    board_table = s.select(
+        Values(
+            s.column('board_id', UUID), 
+            s.column('creation_date', s.Date), 
+            s.column('name', s.Text), 
+            s.column('description', s.Text), 
+            s.column('artwork_url', s.Text), 
+            s.column('last_modified_timestamp', s.BigInteger), 
+            s.column('board_type', s.Text), 
+            name='tmp'
+        ).data([(board_args['board_id'], cast(board_args['creation_date'], s.Date), board_args['name'], 
+                 board_args['description'], board_args['last_modified_timestamp'], board_args['artwork_url'], 
+                 board_args['board_type'])])
+    ).cte()
+    
+    user_board_table = s.select(
+        Values(
+            s.column('board_id', UUID), 
+            s.column('user_id', s.BigInteger), 
+            s.column('last_modified_timestamp', s.BigInteger), 
+            s.column('is_owner', s.Boolean), 
+            s.column('is_collaborator', s.Boolean), 
+            s.column('is_following', s.Boolean), 
+            s.column('is_suggested', s.Boolean), 
+            name='tmp'
+        ).data([(user_board_args['board_id'], user_board_args['user_id'], user_board_args['last_modified_timestamp'],
+                user_board_args['is_owner'], user_board_args['is_collaborator'], user_board_args['is_following'],
+                user_board_args['is_suggested'])])
+    ).cte()
+    
+    board_select_stmt = s.select(board_table).where(
+        ~s.exists([get_price_drop_boards_for_user_id_cte.c.board_type]) \
+            .where(get_price_drop_boards_for_user_id_cte.c.board_type == 'price_drop')
+    ).cte() 
+    user_board_select_stmt = s.select(user_board_table).where(
+        ~s.exists([get_price_drop_boards_for_user_id_cte.c.board_type]) \
+            .where(get_price_drop_boards_for_user_id_cte.c.board_type == 'price_drop')
+    ).cte()
+    
+    board_insert_statement = s.insert(p.Board).from_select(list(board_args.keys()) , board_select_stmt)
+    user_board_insert_statement = s.insert(p.UserBoard).from_select(list(user_board_args.keys()) , user_board_select_stmt)
+
+    ## Execute session transaction
+    try:
+        with session_scope() as session:
+            session.execute(board_insert_statement)
+            session.execute(user_board_insert_statement)
+            session.commit()
+    except Exception as e:
+        print(e)
+        return {"success": False}
+    
+    return {"success": True}
 
 def update_board_name(args: dict) -> dict:
     board_id = args['board_id']
