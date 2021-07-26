65968c8d186fe336287505ef9df8f3354738ffe9
Kian <ghodouss@usc.edu>
2021-07-26 11:28:23 -0500

    return full board info on create board and create user suggested board

--- src/write_user_boards.py
+++ src/write_user_boards.py
@@ -8,6 +8,7 @@ from datetime import datetime as dt
 from sqlalchemy.dialects.postgresql import insert, UUID
 import sqlalchemy as s
 from sqlalchemy.exc import IntegrityError
+from src import read_user_boards
 
 def create_new_board(args: dict) -> dict:
     board_id = uuid.uuid4().hex
@@ -122,11 +123,15 @@ def create_user_suggested_board(args: dict) -> dict:
             session.add(board_smart_tag)
             session.flush()
             session.execute(insert_board_product_stmt)
+            session.commit()
     except Exception as e:
         print(e)
         return {"success": False}
 
-    return {"success": True, "board_id": board_id}
+    return {
+        "success": True, 
+        **read_user_boards.getBoardInfo({'board_id': board_id})
+    }
 
 def update_board_name(args: dict) -> dict:
     board_id = args['board_id']
