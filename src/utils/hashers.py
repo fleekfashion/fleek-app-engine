73e3448dd41cac7d219c83b668f655c32c1ddac9
Kian <ghodouss@usc.edu>
2021-07-26 11:21:11 -0500

    filter out price drop in add to board options

--- src/utils/hashers.py
+++ src/utils/hashers.py
@@ -1,5 +1,8 @@
 import pyhash
+from src.defs import postgres as p
 
 def apple_id_to_user_id_hash(apple_id):
+    if p.DEV_MODE:
+        return apple_id
     hasher = pyhash.farm_fingerprint_64()
     return hasher(apple_id) // 10 if apple_id is not None else None
