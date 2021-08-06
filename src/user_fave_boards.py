b65fef8d1a8497930d49d796862a9ba80284b64b
Rami <rhammoud@usc.edu>
2021-08-05 19:44:10 -0700

    Api endpoint for getUserBag

--- src/user_fave_boards.py
+++ src/user_fave_boards.py
@@ -4,20 +4,20 @@ from src.utils.sqlalchemy_utils import run_query
 from src.utils import hashers
 from src.defs import postgres as p
 
-def getUserFaveProductBatch(args: dict) -> dict:
+def get_ordered_user_product_action_table_batch(table: s.Table, args: dict) -> dict:
     user_id = hashers.apple_id_to_user_id_hash(args['user_id'])
-    limit = args['limit']
-    offset = args['offset']
+    limit = args.get('limit', None)
+    offset = args.get('offset', 0)
 
-    user_fave_pids_query = s.select(
-        p.UserProductFaves.product_id,
-        p.UserProductFaves.event_timestamp.label('last_modified_timestamp')
+    pids_query = s.select(
+        table.product_id,
+        table.event_timestamp.label('last_modified_timestamp')
     ) \
-        .filter(p.UserProductFaves.user_id == user_id) \
+        .filter(table.user_id == user_id) \
         .cte()
 
     products_batch_ordered = board.get_ordered_products_batch(
-        user_fave_pids_query, 
+        pids_query, 
         'last_modified_timestamp', 
         args
     ) \
@@ -29,6 +29,12 @@ def getUserFaveProductBatch(args: dict) -> dict:
         "products": result
     }
 
+def getUserFaveProductBatch(args: dict) -> dict:
+    return get_ordered_user_product_action_table_batch(p.UserProductFaves, args)
+
+def getUserBag(args: dict) -> dict:
+    return get_ordered_user_product_action_table_batch(p.UserProductBags, args)
+    
 def getUserFaveStats(args: dict) -> dict:
     user_id = hashers.apple_id_to_user_id_hash(args['user_id'])
 
