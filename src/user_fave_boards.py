fd0412d46e8662147861d47c67cc4c97d5aba6c4
Rami <rhammoud@usc.edu>
2021-08-05 20:51:41 -0700

    Decouple getUserBag and getUserFaveProductBatch

--- src/user_fave_boards.py
+++ src/user_fave_boards.py
@@ -4,20 +4,20 @@ from src.utils.sqlalchemy_utils import run_query
 from src.utils import hashers
 from src.defs import postgres as p
 
-def get_ordered_user_product_action_table_batch(table: s.Table, args: dict) -> dict:
+def getUserFaveProductBatch(args: dict) -> dict:
     user_id = hashers.apple_id_to_user_id_hash(args['user_id'])
-    limit = args.get('limit', None)
-    offset = args.get('offset', 0)
+    limit = args['limit']
+    offset = args['offset']
 
-    pids_query = s.select(
-        table.product_id,
-        table.event_timestamp.label('last_modified_timestamp')
+    user_fave_pids_query = s.select(
+        p.UserProductFaves.product_id,
+        p.UserProductFaves.event_timestamp.label('last_modified_timestamp')
     ) \
-        .filter(table.user_id == user_id) \
+        .filter(p.UserProductFaves.user_id == user_id) \
         .cte()
 
     products_batch_ordered = board.get_ordered_products_batch(
-        pids_query, 
+        user_fave_pids_query, 
         'last_modified_timestamp', 
         args
     ) \
@@ -29,12 +29,6 @@ def get_ordered_user_product_action_table_batch(table: s.Table, args: dict) -> d
         "products": result
     }
 
-def getUserFaveProductBatch(args: dict) -> dict:
-    return get_ordered_user_product_action_table_batch(p.UserProductFaves, args)
-
-def getUserBag(args: dict) -> dict:
-    return get_ordered_user_product_action_table_batch(p.UserProductBags, args)
-    
 def getUserFaveStats(args: dict) -> dict:
     user_id = hashers.apple_id_to_user_id_hash(args['user_id'])
 
@@ -47,3 +41,25 @@ def getUserFaveStats(args: dict) -> dict:
     return {
         "stats": result
     }
+
+def getUserBag(args: dict) -> dict:
+    user_id = hashers.apple_id_to_user_id_hash(args['user_id'])
+
+    user_bag_pids_query = s.select(
+        p.UserProductBags.product_id,
+        p.UserProductBags.event_timestamp.label('last_modified_timestamp')
+    ) \
+        .filter(p.UserProductBags.user_id == user_id) \
+        .cte()
+
+    products_batch_ordered = board.get_ordered_products_batch(
+        user_bag_pids_query, 
+        'last_modified_timestamp', 
+        args
+    )
+    
+    result = run_query(products_batch_ordered)
+    return {
+        "products": result
+    }
+    
