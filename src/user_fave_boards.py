22634b9b40ee1899464460e83323716c2a1e80c5
Rami Hammoud <rhammoud@usc.edu>
2021-08-12 01:28:25 -0700

    Add product previews to getUserFaveStats (#167)
    * Add product previews to getUserFaveStats
    
    * Merge stats and products into one db call

--- src/user_fave_boards.py
+++ src/user_fave_boards.py
@@ -32,14 +32,28 @@ def getUserFaveProductBatch(args: dict) -> dict:
 def getUserFaveStats(args: dict) -> dict:
     user_id = hashers.apple_id_to_user_id_hash(args['user_id'])
 
-    user_fave_pids_query = s.select(p.UserProductFaves.product_id) \
+    user_fave_pids_query = s.select(
+        s.literal('all_faves').label('board_id'), # Dummy value
+        p.UserProductFaves.product_id,
+        p.UserProductFaves.event_timestamp.label('last_modified_timestamp')
+    ) \
         .filter(p.UserProductFaves.user_id == user_id) \
         .cte()
-    get_product_group_stats_query = board.get_product_group_stats(user_fave_pids_query, None)
+    get_product_group_stats_query = board.get_product_group_stats(user_fave_pids_query, None).cte()
+    product_previews = board.get_product_previews(
+        user_fave_pids_query, 
+        'board_id',
+        'last_modified_timestamp'
+    ).cte()
     
-    result = run_query(get_product_group_stats_query)
+    # Since both queries are one row, can merge into one db call
+    join_queries = s.select(get_product_group_stats_query, product_previews).join(product_previews, s.true())
+
+    result = run_query(join_queries)
+    result = result[0] if len(result) > 0 else {}
+    for dummy_key in ['temp_id', 'board_id']: result.pop(dummy_key, None)
     return {
-        "stats": result
+        "stats_and_products": result
     }
 
 def getUserBag(args: dict) -> dict:
