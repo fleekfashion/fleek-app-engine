7d324b45acf3a488206a08163ef9cfc9a6508890
Kian <ghodouss@usc.edu>
2021-10-24 14:48:40 -0700

    add daily mix function

--- src/explore_boards.py
+++ src/explore_boards.py
@@ -13,29 +13,34 @@ from src.utils import hashers
 from src.defs import postgres as p
 from src.defs.types.board_type import BoardType
 
-def get_fave_smart_tags(user_id) -> Select:
-    products = s.select(
+def get_fave_smart_tags(user_id: int, limit: int, power: float=2) -> Select:
+    t = s.select(
         p.UserProductFaves,
         F.setseed(qutils.get_daily_random_seed()),
+    ).cte()
+
+    products = s.select(
+        t,
+        F.random().label('res'),
         ) \
-        .where(p.UserProductFaves.user_id == user_id) \
+        .where(t.c.user_id == user_id) \
         .order_by(
             F.random() *
             F.power(
                 (
                     qutils.days_ago_timestamp(0) -
-                    p.UserProductFaves.event_timestamp
+                    t.c.event_timestamp
                 ) / (24*60*60)
                 + 1,
-                2
+                power
             ).asc()
         ) \
-        .limit(100) \
+        .limit(limit) \
         .cte()
 
     smart_tags = s.select(
         p.ProductSmartTag.smart_tag_id,
-        F.count(p.ProductSmartTag.smart_tag_id).label('c')
+        F.count(p.ProductSmartTag.smart_tag_id).label('c'),
     ).where(p.ProductSmartTag.product_id == products.c.product_id) \
         .group_by(p.ProductSmartTag.smart_tag_id)
     return smart_tags
@@ -75,15 +80,18 @@ def getExploreBoardsBatch(args):
     limit = args['limit']
 
     smart_tags = qutils.union_by_names(
-        get_fave_smart_tags(user_id),
+        get_fave_smart_tags(user_id, 100),
         get_random_smart_tags()
+    ).cte()
+    smart_tags = s.select(
+        smart_tags,
+        F.setseed(qutils.get_daily_random_seed()),
     ).cte()
 
     processed_tags = s.select(
         smart_tags,
         p.SmartTag.suggestion.label('name'),
         p.SmartTag.product_label.label('product_label'),
-        F.setseed(qutils.get_daily_random_seed()),
         F.row_number() \
             .over(
                 order_by=(
@@ -130,4 +138,43 @@ def getExploreBoardsBatch(args):
 
     return {
         "smart_tag_boards": parsed_boards
+    }
+
+
+def _get_daily_mix_products(user_id):
+    smart_tags = get_fave_smart_tags(user_id, 100, 1).cte()
+    top_smart_tags = s.select(
+        smart_tags,
+    ).order_by( 
+        (smart_tags.c.c).desc()
+    ).limit(50) \
+        .cte()
+
+    relevent_products = _get_product_smart_tag(top_smart_tags) \
+        .distinct(literal_column('product_id')) \
+        .order_by(literal_column('product_id')) \
+        .cte()
+    products = qutils.join_product_info(relevent_products).cte()
+
+    res = qutils.apply_ranking(
+        products,
+        user_id,
+        pct=1.6,
+        random_seed=qutils.get_daily_random_seed()
+    ).limit(100)
+    return res
+
+def getDailyMixProductsBatch(args):
+    user_id = hashers.apple_id_to_user_id_hash(args['user_id'])
+    offset = args['offset']
+    limit = args['limit']
+    products = _get_daily_mix_products(user_id).cte()
+
+    batch = s.select(products) \
+        .offset(offset) \
+        .limit(limit)
+
+    res = run_query(batch)
+    return {
+        'products': res
     }
